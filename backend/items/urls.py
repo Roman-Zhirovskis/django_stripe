943a17160055f9b1b41a0dc6cfce72dfb949d5da
Roman-Zhirovskis <tesslogun@gmail.com>
2023-12-06 23:28:36 +0300

    little urls changes

--- backend/items/urls.py
+++ backend/items/urls.py
@@ -5,7 +5,7 @@ from django.urls import path
 urlpatterns = [
     path("item/<int:id>", ItemDetailView.as_view(), name="item_detail"),
     # Это нужно перенести в приложение payments, как и логику сервисов
-    path("item_buy/<int:id>", ItemRetriveView.as_view(), name="buy_item"),
+    path("buy/<int:id>", ItemRetriveView.as_view(), name="buy_item"),
     path("order_buy/<int:id>", OrderRetriveView.as_view(), name="make_order"),
     # path("order_tax/<int:id>", OrderTaxRetriveView.as_view(), name="tax_order"),
 ]
