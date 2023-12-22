d6b7006eb3b3d2e2675dc4afe98043a3d5576680
Roman-Zhirovskis <tesslogun@gmail.com>
2023-12-22 17:34:54 +0300

    append orders module| init models, admin.migrations in

--- backend/orders/admin.py
+++ backend/orders/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from orders.models import Order
+
+
+class OrderItemsInline(admin.TabularInline):
+    model = Order.items.through
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (OrderItemsInline,)
+
+    readonly_fields = (
+        "payment_status",
+        "items",
+    )
--- backend/orders/models.py
+++ backend/orders/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+from items.models import Item
+from financial_units.models import Discount, Tax
+
+
+class Order(models.Model):
+    items = models.ManyToManyField(Item, blank=True)
+    payment_status = models.BooleanField(default=False)
+    discount = models.ForeignKey(Discount, on_delete=models.CASCADE, blank=True, null=True)
+    tax = models.ForeignKey(Tax, on_delete=models.CASCADE, blank=True, null=True)
+
+    def __str__(self):
+        return f"Order {self.id}"
+
+    def calculate_total_cost(self):
+        return sum(item.price for item in self.items.all())
